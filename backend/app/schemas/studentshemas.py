7eaf0caf369b8d40cbac000123e27c1805b7c78a
swadhinbiswas <swadhinbiswas.cse@gmail.com>
2024-04-30 22:58:58 +0600

    update

--- backend/app/schemas/studentshemas.py
+++ backend/app/schemas/studentshemas.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 from typing import Optional
+
 class StudentBase(BaseModel):
     id: int
     name: str
---
+++ backend/app/settings/setting.py
@@ -0,0 +1,22 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+class Setting:
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    DEBUG = os.getenv('DEBUG')
+    ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
+    DATABASES_URL = os.getenv('DATABASES_URL')
+    DATABASE_HOST = os.getenv('DATABASE_HOST')
+    DATABASE_PORT = os.getenv('DATABASE_PORT')
+    DATABASE_NAME = os.getenv('DATABASE_NAME')
+    DATABASE_USER = os.getenv('DATABASE_USER')
+    DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
+    DATABASE_ENGINE = os.getenv('DATABASE_ENGINE')
+    
+    
+    
+
+setting = Setting()
+    
